44b7b97b5f30605e3b16f623635de06ce0fa68a7
Boris Gurevich <gb991642@gmail.com>
2021-05-28 12:13:45 +0300

    docker-compose created,running MySQL.flask working.

--- Weight/app.py
+++ Weight/app.py
@@ -1,26 +1,40 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
-from sqlalchemy import inspect
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import create_engine
+# from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+# from sqlalchemy import inspect
+# from sqlalchemy.sql import text
+# import os
+
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
-import os
+
+
 
 app = Flask(__name__)
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:8088/Weight'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:8088/Weight'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
-now = datetime.now() # current date and time
+now = datetime.now()  # current date and time
 date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
+
 
 @app.route("/", methods=["GET"])
 def index():
-    #db.engine.execute('mysql -V')
-    return "This time is: " + date_time
+    # db.engine.execute('mysql -V')
+    return render_template("index.html")
+@app.route("/blog.html", methods=["POST","GET"])
+def index1():
+    return render_template("blog.html")
+@app.route("/batch-weight.html", methods=["POST","GET"])
+def index2():
+    return render_template("batch-weight.html")
+
+@app.route("/index.html", methods=["POST","GET"])
+def index3():
+    return render_template("index.html")
 
 @app.route("/health", methods=["GET"])
 def health():
@@ -30,5 +44,6 @@ def health():
     except:
         return '<h1>Status Code: 500.</h1>'
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8088 ,debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1")
