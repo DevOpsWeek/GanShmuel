82974c4777367458949740b31e4716fbf3a4a217
Ubuntu <ubuntu@ip-172-31-17-160.eu-central-1.compute.internal>
2021-05-29 17:13:25 +0000

    changed userdata 11X

--- DevOps/folder_app/application.py
+++ DevOps/folder_app/application.py
@@ -10,7 +10,7 @@ def create_docker_compose(command_list,branch_name):
     print(f"------- worked on branch {branch_name} -------")
 
 def run_docker(branch_name):
-    command_list=["exit",f"git checkout --track origin/{branch_name}","docker run hello-world"]
+    command_list=["docker run --privileged --pid=host -it alpine:3.8 nsenter -t 1 -m -u -n -i bash",f"git checkout --track origin/{branch_name}","docker run hello-world"]
     if branch_name=="DevOps" or branch_name=="Weight" or branch_name=="Billing":
         create_docker_compose(command_list,branch_name)
         print("exucuted the docker compose file ! ")
@@ -30,4 +30,3 @@ def webhook():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True, threaded=False)
-
