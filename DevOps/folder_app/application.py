d47e73f0c38b921254fb7ed8d1d9443d1fb17dbc
Eyal159 <eyal2561999@gmail.com>
2021-05-29 15:59:24 +0300

    updated app.py

--- DevOps/folder_app/application.py
+++ DevOps/folder_app/application.py
@@ -1,12 +1,31 @@
 import json
 from flask import Flask, request
-
 app = Flask(__name__)
+
+
+def run_docker(branch_name):
+    if branch_name=="DevOps":
+        pass
+    elif branch_name=="Weight":
+        pass
+    elif branch_name=="Billing":
+        pass
+    elif branch_name=='main':
+        pass
+    else:
+        return "DIDNT GET ANY BRANCH NAME !"
+
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    str1="200"
-    return str1
+    json_str=request.json
+    dict_str=json.loads(json_str)
+    current_branch=list(dict_str['ref'].split("/"))
+    print(current_branch)
+    fun_result=run_docker(current_branch)
+    return fun_result
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True, threaded=False) 
