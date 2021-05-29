3cc1187427534328b52eb7ca80353be2333ba957
Ubuntu <ubuntu@ip-172-31-17-160.eu-central-1.compute.internal>
2021-05-29 13:59:32 +0000

    updated app.py

--- DevOps/folder_app/application.py
+++ DevOps/folder_app/application.py
@@ -1,18 +1,28 @@
-
-import json
+import json,os
 from flask import Flask, request
 app = Flask(__name__)
 
+def create_docker_compose(command_list,branch_name):
+    os.system(command_list[0])
+    os.system(command_list[1])
+    os.system(command_list[2])
+    os.system(command_list[3])
+    print(f"------- worked on branch {branch_name}-------------")
 
 def run_docker(branch_name):
+    command_list=["git clone https://github.com/DevOpsWeek/GanShmuel.git","cd GanShmuel",f"git checkout --track origin/{branch_name}","docker run -it hello-world"]
     if branch_name=="DevOps":
-        pass
+        create_docker_compose(command_list,branch_name)
+        return ("here Devops")
     elif branch_name=="Weight":
-        pass
+        create_docker_compose(command_list,branch_name)
+        return("here Weight")
     elif branch_name=="Billing":
-        pass
+        create_docker_compose(command_list,branch_name)
+        return("here Billing")
     elif branch_name=='main':
-        pass
+        create_docker_compose(command_list,branch_name)
+        return("here main")
     else:
         return "DIDNT GET ANY BRANCH NAME !"
 
@@ -22,7 +32,7 @@ def webhook():
     json_str=request.json
     current_branch=list(json_str['ref'].split("/"))
     print(current_branch[2])
-    fun_result=run_docker(current_branch)
+    fun_result=run_docker(current_branch[2])
     print(fun_result)
     return fun_result
 
@@ -30,3 +40,4 @@ def webhook():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True, threaded=False)
+
