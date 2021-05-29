1a2084f293bfb5ac964f6dcd592be457370c549c
Roy <roy.zhr@outlook.com>
2021-05-29 17:02:52 +0300

    created base app.py and corrected db sql

--- app/app.py
+++ app/app.py
@@ -36,57 +36,57 @@ def health():
     results = cursor.fetchall()
     cursor.close()
     connection.close()
-
     return results
 
-@app.route('/')
-def index():
-     return render_template('index.html')
+def providers() -> List[Dict]:
+    connection=conn_db()
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM Providers')
+    results = [{id: name} for (id, name) in cursor]
+    cursor.close()
+    connection.close()
+    return results
 
-@app.route('/trucks')
+@app.route('/',methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/health',methods=['GET'])
+def getHealth():
+    return json.dumps(health())
+
+@app.route('/providers', methods=['GET'])
+def postProvider():
+    return json.dumps({'providers':providers()})
+
+
+# @app.route("/rates", methods=["GET", "POST"])
+# def getPOSTRates():
+#     if request.method == 'GET':
+#         return "Showig rates"
+    
+#     if request.method == 'POST':
+#         product_id = request.form['product_id']
+#         rate = request.form['rate']
+#         scope = request.form['scope']
+#         cursor = mysql.get_db().cursor()
+#         cursor.execute(''' INSERT INTO rates_table VALUES(%s,%s)''',(product_id,rate,scope))
+#         mysql.get_db.commit()
+#         cursor.close()
+#         return f"Done!!"
+
+@app.route("/getTrucks", methods=["GET"])
 def getTrucks() -> str:
     return json.dumps({'trucks': trucks()})
 
-@app.route('/health')
-def getHealth()->str:
-    return json.dumps(health())
 
-@app.route("/provider", methods=["POST"])
-def postProvider():
-    return "provider"
-
-
-@app.route('/form')
-def form():
-    return 'imagine this is a form'
-
-@app.route("/rates", methods=["GET", "POST"])
-def getPOSTRates():
-    if request.method == 'GET':
-        return "Showig rates"
-    
-    if request.method == 'POST':
-        product_id = request.form['product_id']
-        rate = request.form['rate']
-        scope = request.form['scope']
-        cursor = mysql.get_db().cursor()
-        cursor.execute(''' INSERT INTO rates_table VALUES(%s,%s)''',(product_id,rate,scope))
-        mysql.get_db.commit()
-        cursor.close()
-        return f"Done!!"
-
-@app.route("/truck", methods=["POST"])
-def postTruck():
-    return "truck"
-
-@app.route("/truck", methods=["PUT"])
-def putTruck():
-    return "truck"
+# @app.route("/truck", methods=["PUT"])
+# def putTruck():
+#     return "truck"
 
 @app.route("/bill", methods=["GET"])
 def getBill():
     return render_template('bill.html')
-
 
 
 if __name__ == '__main__':
--- db/billingdb.sql
+++ db/billingdb.sql
@@ -11,7 +11,7 @@ USE `billdb`;
 -- Table structure
 --
 
-CREATE TABLE IF NOT EXISTS `Provider` (
+CREATE TABLE IF NOT EXISTS `Providers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -21,24 +21,24 @@ CREATE TABLE IF NOT EXISTS `Rates` (
   `product_id` varchar(50) NOT NULL,
   `rate` int(11) DEFAULT 0,
   `scope` varchar(50) DEFAULT NULL,
-  FOREIGN KEY (scope) REFERENCES `Provider`(`id`)
+  FOREIGN KEY (scope) REFERENCES `Providers`(`id`)
 ) ENGINE=MyISAM ;
 
 CREATE TABLE IF NOT EXISTS `Trucks` (
   `id` varchar(10) NOT NULL,
   `provider_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`provider_id`) REFERENCES `Provider`(`id`)
+  FOREIGN KEY (`provider_id`) REFERENCES `Providers`(`id`)
 ) ENGINE=MyISAM ;
 
 --
 -- Dumping data
 --
---  INSERT INTO Provider (`id`,`name`) VALUES ('ALL'),('pro1'),
--- (3, 'pro2');
+INSERT INTO Providers (`id`,`name`) VALUES (1,'pro1'),
+(2, 'pro2');
 
 INSERT INTO Rates (`product_id`, `rate`, `scope`) VALUES ('1', 2, 'ALL'),
-(2, 4, 'pro1');
+('2', 4, 'pro1');
 
 INSERT INTO Trucks (`id`, `provider_id`) VALUES ('134-33-443', 2),
 ('222-33-111', 1);
