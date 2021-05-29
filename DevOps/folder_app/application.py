8c1ecae2c0019312fe15b2b74315b2884e7f51d5
Ubuntu <ubuntu@ip-172-31-17-160.eu-central-1.compute.internal>
2021-05-29 16:31:54 +0000

    changed userdata 8X

--- DevOps/folder_app/application.py
+++ DevOps/folder_app/application.py
@@ -3,29 +3,19 @@ from flask import Flask, request
 app = Flask(__name__)
 
 def create_docker_compose(command_list,branch_name):
-    os.system(command_list[0])
-    os.system(command_list[1])
-    os.system(command_list[2])
-    os.system(command_list[3])
-    print(f"------- worked on branch {branch_name}-------------")
+    for i in command_list:
+        os.system(command_list[i])
+    print(f"------- worked on branch {branch_name} -------")
 
 def run_docker(branch_name):
-    command_list=["git clone https://github.com/DevOpsWeek/GanShmuel.git","cd GanShmuel",f"git checkout --track origin/{branch_name}","docker run -it hello-world"]
-    if branch_name=="DevOps":
+    command_list=[f"git checkout --track origin/{branch_name}","docker run hello-world"]
+    if branch_name=="DevOps" or branch_name=="Weight" or branch_name=="Billing":
         create_docker_compose(command_list,branch_name)
-        return ("here Devops")
-    elif branch_name=="Weight":
-        create_docker_compose(command_list,branch_name)
-        return("here Weight")
-    elif branch_name=="Billing":
-        create_docker_compose(command_list,branch_name)
-        return("here Billing")
-    elif branch_name=='main':
-        create_docker_compose(command_list,branch_name)
-        return("here main")
+        print("exucuted the docker compose file ! ")
+        return "exucuted the docker compose file ! "
     else:
+        print("DIDNT GET ANY BRANCH NAME !")
         return "DIDNT GET ANY BRANCH NAME !"
-
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -36,8 +26,5 @@ def webhook():
     print(fun_result)
     return fun_result
 
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True, threaded=False)
-
