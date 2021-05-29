5a141fd3d935b82780184f6fa4f38cb761a8276e
Ubuntu <ubuntu@ip-172-31-17-160.eu-central-1.compute.internal>
2021-05-29 13:28:02 +0000

    add nothing to userdata again again again

--- DevOps/folder_app/application.py
+++ DevOps/folder_app/application.py
@@ -1,3 +1,4 @@
+
 import json
 from flask import Flask, request
 app = Flask(__name__)
@@ -20,11 +21,12 @@ def run_docker(branch_name):
 def webhook():
     json_str=request.json
     current_branch=list(json_str['ref'].split("/"))
-    print(current_branch)
+    print(current_branch[2])
     fun_result=run_docker(current_branch)
+    print(fun_result)
     return fun_result
 
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True, threaded=False) 
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=False)
