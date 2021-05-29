532541f8889bdec2d61c436ccb4af529727cb797
Ubuntu <ubuntu@ip-172-31-17-160.eu-central-1.compute.internal>
2021-05-29 16:43:54 +0000

    changed userdata 9X

--- DevOps/folder_app/application.py
+++ DevOps/folder_app/application.py
@@ -3,12 +3,14 @@ from flask import Flask, request
 app = Flask(__name__)
 
 def create_docker_compose(command_list,branch_name):
+    com="ls"
+    os.system(com)
     for i in command_list:
         os.system(i)
     print(f"------- worked on branch {branch_name} -------")
 
 def run_docker(branch_name):
-    command_list=[f"git checkout --track origin/{branch_name}","docker run hello-world"]
+    command_list=["exit",f"git checkout --track origin/{branch_name}","docker run hello-world"]
     if branch_name=="DevOps" or branch_name=="Weight" or branch_name=="Billing":
         create_docker_compose(command_list,branch_name)
         print("exucuted the docker compose file ! ")
@@ -28,3 +30,4 @@ def webhook():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True, threaded=False)
+
