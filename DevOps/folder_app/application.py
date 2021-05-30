e261b696e0b95ab9c2e82276b48c0cd2f862aa9d
Ubuntu <ubuntu@ip-172-31-17-160.eu-central-1.compute.internal>
2021-05-30 11:01:44 +0000

    updated up to 30/5 14:00

--- DevOps/folder_app/application.py
+++ DevOps/folder_app/application.py
@@ -8,12 +8,9 @@ def create_docker_compose(command_list,branch_name):
     print(f"------- worked on branch {branch_name} -------")
 
 def run_docker(branch_name):
-    #command_list=[f"git checkout --track origin/{branch_name}",f"cd {branch_name}","docker compose up ."]
+    command_list=[f"git checkout --track origin/{branch_name}",f"cd {branch_name}","docker compose up ."]
     if branch_name=="DevOps" or branch_name=="Weight" or branch_name=="Billing":
-        #create_docker_compose(command_list,branch_name)
-        cmd="ls"
-        os.system(cmd)
-        subprocess.call("script.sh") 
+        create_docker_compose(command_list,branch_name)
         print("exucuted the docker compose file ! ")
         return "exucuted the docker compose file ! "
     else:
