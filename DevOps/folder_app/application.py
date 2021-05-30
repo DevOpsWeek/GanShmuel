7e174714c3729d8ffa7f73ced9576f646b95bb35
Ubuntu <ubuntu@ip-172-31-17-160.eu-central-1.compute.internal>
2021-05-30 14:56:31 +0000

    plz

--- DevOps/folder_app/application.py
+++ DevOps/folder_app/application.py
@@ -12,7 +12,8 @@ def create_docker_compose(command_list,branch_name):
 
 def run_docker(branch_name):
     branch_lower=branch_name.lower()  
-    command_list=["git clone https://github.com/DevOpsWeek/GanShmuel.git","ls",f"docker rm $(docker stop $(docker ps -a -q --filter=\"name={branch_lower}-container\"))",f"git checkout --track origin/{branch_name}",f"{branch_name}/folder_app",f"docker build -t image/{branch_lower} .",f"docker run -d --name {branch_lower}-container image/{branch_lower}"]
+    os.system("git clone https://github.com/DevOpsWeek/GanShmuel.git")
+    command_list=["ls",f"docker rm $(docker stop $(docker ps -a -q --filter=\"name={branch_lower}-container\"))",f"git checkout --track origin/{branch_name}",f"{branch_name}/folder_app",f"docker build -t image/{branch_lower} .",f"docker run -d --name {branch_lower}-container image/{branch_lower}"]
     if branch_name=="DevOps" or branch_name=="Weight" or branch_name=="Billing":
         os.chdir("GanShmuel")
         create_docker_compose(command_list,branch_name)
@@ -30,4 +31,3 @@ def webhook():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True, threaded=False)
-
