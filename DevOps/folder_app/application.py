52906867f30238217a789e1c1e2479c9e62035ca
Ubuntu <ubuntu@ip-172-31-17-160.eu-central-1.compute.internal>
2021-05-30 14:46:33 +0000

    plz

--- DevOps/folder_app/application.py
+++ DevOps/folder_app/application.py
@@ -7,7 +7,7 @@ os.system(clone_repo)
 
 def create_docker_compose(command_list,branch_name):
     for i in command_list:
-        if i=="DevOps/folder_app":
+        if i==f"{branch_name}/folder_app":
             os.chdir(i)
         else:
             os.system(i)
@@ -15,25 +15,23 @@ def create_docker_compose(command_list,branch_name):
 
 def run_docker(branch_name):
     branch_lower=branch_name.lower()
-    print(branch_lower)
-    command_list=["ls","pwd",f"git checkout --track origin/{branch_name}","DevOps/folder_app","ls",f"docker build -t image/{branch_lower} .",f"docker run -d image/{branch_lower}"]
+    
+    command_list=[f"docker rm $(docker stop $(docker ps -a -q --filter=\"name={branch_lower}-container\"))",f"git checkout --track origin/{branch_name}",f"{branch_name}/folder_app",f"docker build -t image/{branch_lower} .",f"docker run -d --name {branch_lower}-container image/{branch_lower}"]
     if branch_name=="DevOps" or branch_name=="Weight" or branch_name=="Billing":
         os.chdir("GanShmuel")
         create_docker_compose(command_list,branch_name)
         print("exucuted the docker compose file ! ")
-        return "exucuted the docker compose file ! "
     else:
         print("DIDNT GET ANY BRANCH NAME !")
-        return "DIDNT GET ANY BRANCH NAME !"
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     json_str=request.json
     current_branch=list(json_str['ref'].split("/"))
     print(current_branch[2])
-    fun_result=run_docker(current_branch[2])
-    print(fun_result)
-    return "test"
+    run_docker(current_branch[2])
+    return "200"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True, threaded=False)
+
