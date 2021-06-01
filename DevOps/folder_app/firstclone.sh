318a3d1d3704bd35df015c38bc1b30a300c1ea51
Eyal159 <74238936+Eyal159@users.noreply.github.com>
2021-06-01 10:21:36 +0300

    Create firstclone.sh

---
+++ DevOps/folder_app/firstclone.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+git clone https://github.com/DevOpsWeek/GanShmuel.git
+cd GanShmuel
+git checkout --track origin/Billing
+git checkout --track origin/Weight
+git checkout --track origin/DevOps
