a200db62d8909f1522a36dc087f959ee60026d72
Eyal159 <74238936+Eyal159@users.noreply.github.com>
2021-06-01 10:34:24 +0300

    Delete firstclone.sh

--- DevOps/folder_app/firstclone.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-git clone https://github.com/DevOpsWeek/GanShmuel.git
-cd GanShmuel
-git checkout --track origin/Billing
-git checkout --track origin/Weight
-git checkout --track origin/DevOps
